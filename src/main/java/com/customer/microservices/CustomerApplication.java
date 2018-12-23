c940166d4c6ccfb92c2e5c3a40535ab4692837c0
user <user@hp>
2018-12-24 01:02:06 +0530

    Eureka Discovery client

--- src/main/java/com/customer/microservices/CustomerApplication.java
+++ src/main/java/com/customer/microservices/CustomerApplication.java
@@ -2,8 +2,10 @@ package com.customer.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(scanBasePackages={"com.customer.microservices.*"})
+@SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerApplication {
 
 	public static void main(String[] args) {
